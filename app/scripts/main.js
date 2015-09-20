e6c553d61e3fb54f739ed17591c5d47473a6fe86
Alex Macmillan <alex@x13n.com>
2015-09-19 23:42:38 -0400

    Zooming cards
    When you click or tap a card, it zooms to fill the viewport.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,57 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+$(function(){
+  var ANIMATION_DURATION = 200;
+  
+  $(".cards .card article").click(function(){
+    
+    var scrollTop = $(document.body).scrollTop();
+    var scrollLeft = $(document.body).scrollLeft();
+    
+    var $card = $(this).parent();
+    var $cardarticle = $card.find("article");
+    
+    var $overlaycard = $card.clone().css({opacity: 0});
+    var $overlayarticle = $overlaycard.find("article");
+    
+    var $overlay = $("<div class='overlay'></div>")
+      .append($overlaycard)
+      .appendTo(document.body)
+      .css({
+        opacity: 0
+      })
+      .animate({
+        opacity: 1
+      }, ANIMATION_DURATION)
+      .click(function(){
+        $(this).remove();
+        $card.removeClass("activated");
+      }); 
+    
+    var $zoomcard = $card.clone();
+    
+    var $zoom = $("<div class='zoom'></div>")
+      .append($zoomcard)
+      .appendTo(document.body);
+
+    $zoomcard
+      .offset($cardarticle.offset())
+      .width($card.width())
+      .height($card.height())
+      .animate({
+        top: $overlayarticle.offset().top - scrollTop,
+        left: $overlayarticle.offset().left - scrollLeft,
+        width: $overlaycard.width(),
+        height: $overlaycard.height()
+      }, ANIMATION_DURATION, "swing", function() {
+        console.log("Animation complete");
+        $zoom.remove();
+        $overlaycard.css({
+          opacity: 1
+        });
+      });
+    
+    $card.addClass("activated");
+    
+  });
+  
+});
